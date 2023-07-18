c8e5cd6b9592f9a1cf1fa8792cf20236f9b65837
Jonah Foster <81132130+JonahFoster@users.noreply.github.com>
2023-07-17 21:34:28 -0400

    Add Animal Kingdom trip

--- src/data.js
+++ src/data.js
@@ -1,13 +1,13 @@
 export default [
     {
         id: 1,
-        title: "Mount Fuji",
-        location: "Japan",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+        title: "Animal Kingdom at Disney World",
+        location: "Orlando, Florida",
+        googleMapsUrl: "https://goo.gl/maps/gGGPsM49KwDeXzRn9",
+        startDate: "6 Jun, 2023",
+        endDate: "",
+        description: "Disney's Animal Kingdom, a 500-acre park in Walt Disney World, Florida, is a unique blend of thrilling rides, live shows, and exotic animals. Opened in 1998, it's the largest Disney theme park worldwide, offering an immersive wildlife experience.",
+        imageUrl: "https://source.unsplash.com/1Uk_bI8OmKs"
     },
     {
         id: 2,
