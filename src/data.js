ef55dc8281c809a60e906622c09ee8cddb7f369f
Jonah Foster <81132130+JonahFoster@users.noreply.github.com>
2023-08-03 10:36:53 -0400

    Add Epcot

--- src/data.js
+++ src/data.js
@@ -3,6 +3,16 @@ import condronBallpark from './images/condron-ballpark.jpg'
 export default [
     {
         id: 1,
+        title: "Epcot",
+        location: "Orlando, Florida",
+        googleMapsUrl: "https://goo.gl/maps/BJXrHopYUxV5poPw5",
+        startDate: "29 July, 2023",
+        endDate: "",
+        description: "Epcot at Walt Disney World celebrates human innovation and global culture. Opened in 1982, it features Future World and World Showcase.",
+        imageUrl: "https://source.unsplash.com/NAJa5rRgwDE"
+    },
+    {
+        id: 2,
         title: "Animal Kingdom",
         location: "Orlando, Florida",
         googleMapsUrl: "https://goo.gl/maps/gGGPsM49KwDeXzRn9",
@@ -12,7 +22,7 @@ export default [
         imageUrl: "https://source.unsplash.com/yX9Xm_TpbHA"
     },
     {
-        id: 2,
+        id: 3,
         title: "2023 CWS Regional",
         location: "Gainesville, Florida",
         googleMapsUrl: "https://goo.gl/maps/jZfE4P9HbcZVUapc9",
@@ -20,35 +30,5 @@ export default [
         endDate: "",
         description: "In their second Gainesville regional game, the Florida Gators lost 5-4 to Texas Tech. Facing elimination, they must now defeat both UConn and Texas Tech to pursue the regional title.",
         imageUrl: condronBallpark
-    },
-    {
-        id: 3,
-        title: "Geirangerfjord",
-        location: "Norway",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "01 Oct, 2021",
-        endDate: "18 Nov, 2021",
-        description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
-        imageUrl: "https://source.unsplash.com/3PeSjpLVtLg"
-    },
-    {
-        id: 4,
-        title: "Mount Fuji",
-        location: "Japan",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
-    },
-    {
-        id: 5,
-        title: "Sydney Opera House",
-        location: "Australia",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "27 May, 2021",
-        endDate: "8 Jun, 2021",
-        description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings.",
-        imageUrl: "https://source.unsplash.com/JmuyB_LibRo"
     },
 ]
